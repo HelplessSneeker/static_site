25b6c0146bf00a9f952d153cc287bec50086fb1c
bfn <benjamin@noessler.at>
2026-01-12 12:39:14 +0100

    added function to generate public folder from static folder

--- src/main.py
+++ src/main.py
@@ -1,9 +1,34 @@
-from src.textnode import TextNode
+import os
+import shutil
 
 
 def main():
-    myNode = TextNode("hi", "plain")
-    print(myNode)
+    generate_public_folder()
+
+
+def generate_public_folder():
+    if os.path.exists("public"):
+        shutil.rmtree("public/")
+    os.mkdir("public")
+
+    copy_files("static/", "public/")
+
+
+def copy_files(path, dest):
+    print(f"working on path: {path}")
+    if os.path.isfile(path):
+        print(f"coping file {path}")
+        shutil.copy(path, dest)
+    else:
+        dir_content = os.listdir(path)
+        for dir in dir_content:
+            cp_path = os.path.join(path, dir)
+            print(f"listing file {dir}")
+            dest_path = os.path.join(dest, dir)
+            if os.path.isdir(cp_path):
+                print(f"creating dir: {dest_path}")
+                os.mkdir(dest_path)
+            copy_files(cp_path, dest_path)
 
 
 main()
